77f6b46621441887915a62412a4367e0202e12e8
Marine Desmier <desmier.marine@bbox.fr>
2022-11-02 14:35:02 +0100

    tp 1

---
+++ src/main/java/fr/diginamic/Region.java
@@ -0,0 +1,57 @@
+package fr.diginamic;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="regions")
+public class Region {
+
+	@Id
+	private Integer id;
+	
+	@Column(name="NOM", length = 30, nullable =false)
+	private String nom;
+	
+	/**
+	 * Constructeur sans argument
+	 */
+	public Region() {
+	
+	}
+
+	/**
+	 * Getter pour l'attribut id 
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * Setter pour l'attribut id
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * Getter pour l'attribut nom 
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * Setter pour l'attribut nom
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+}
